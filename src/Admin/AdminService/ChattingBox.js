0e2528d641216b935513491465b12fe25b4d4ef5
YuLin <andy50152@gmail.com>
2022-11-09 22:57:43 +0800

    客服管理者端功能完成

--- src/Admin/AdminService/ChattingBox.js
+++ src/Admin/AdminService/ChattingBox.js
@@ -9,17 +9,37 @@ function ChattingBox({
   socket,
 }) {
   const [inputContent, setInputContent] = useState('');
-  const [newContent, setNewContent] = useState('');
+  const [newContent, setNewContent] = useState({ newMsg: false, content: {} });
   const { sid, side } = chattingPerson;
 
-  useEffect(() => {
-    socket.addEventListener('message', (e) => {
-      //回傳的資料
-      //let sendData = { "msg": content, "name": map.get(ws).name, "post_side": postSide, "post_sid": postSid, "receive_side": receiveSide, "receive_sid": receiveSid, "time": timeNow /*[,"read",true已讀]*/};
+  //回傳的資料
+  //let sendData = { "msg": content, "name": map.get(ws).name, "post_side": postSide, "post_sid": postSid, "receive_side": receiveSide, "receive_sid": receiveSid, "time": timeNow /*[,"read",true已讀]*/};
 
-      const datas = JSON.parse(e.data);
-      console.log(datas);
-    });
+  /*{
+    "msg": "1",
+    "name": "管理者",
+    "post_side": 4,
+    "post_sid": 101,
+    "receive_side": 1,
+    "receive_sid": 5,
+    "time": "2022-11-09 22:00:50",
+    "self": true
+  }*/
+
+  function receiveMessage(e) {
+    const datas = JSON.parse(e.data);
+    setNewContent({ newMsg: true, content: e.data });
+    console.log(datas);
+  }
+
+  useEffect(() => {
+    // this.scrollIntoView({ behavior: 'auto' });
+    socket.addEventListener('message', receiveMessage);
+    console.log('openListener');
+    return () => {
+      socket.removeEventListener('message', receiveMessage);
+      console.log('closeListener');
+    };
   }, []);
 
   function sendData(text, side, sid) {
@@ -33,28 +53,42 @@ function ChattingBox({
   return (
     <>
       <div className="chattingBox">
-        <div
-          className="pointer"
-          onClick={() => {
-            setChatting(false);
-          }}
-        >
-          X
-        </div>
-        <ChatContent chattingPerson={chattingPerson} />
-        <input
-          value={inputContent}
-          onChange={(e) => {
-            setInputContent(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && inputContent !== '') {
-              console.log('enter');
+        <ChatContent
+          chattingPerson={chattingPerson}
+          newContent={newContent}
+          setNewContent={setNewContent}
+        />
+        <div className="chatInputFrame">
+          <div
+            className="pointer"
+            onClick={() => {
+              setChatting(false);
+            }}
+          >
+            X
+          </div>
+          <input
+            value={inputContent}
+            onChange={(e) => {
+              setInputContent(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && inputContent !== '') {
+                // console.log('enter');
+                sendData(inputContent, side, sid);
+                setInputContent('');
+              }
+            }}
+          />
+          <button
+            onClick={() => {
               sendData(inputContent, side, sid);
               setInputContent('');
-            }
-          }}
-        />
+            }}
+          >
+            send
+          </button>
+        </div>
       </div>
       <div
         className="grayBack"
