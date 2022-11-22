f1fbaf395064a0c120e0d7f0c176646b0e28e28b
YU <andy50152@gmail.com>
2022-11-22 17:28:38 +0800

    1122/1730

--- src/Member/MemberOldOrder/index.js
+++ src/Member/MemberOldOrder/index.js
@@ -1,4 +1,45 @@
+import { useEffect } from 'react';
+import './OldOrder.css';
 function MemberOldOrder() {
-  return <>會員歷史訂單頁</>;
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <p className="marb20">過往訂單</p>
+      <div name="訂單整體外框">
+        {/* //===============================================分隔線================================================ */}
+        <div name="單個訂單外框" className="w100p marb20 oldOrderFrame">
+          <div className="w100p  padV20 padH20 disf oldOrderTopDetail">
+            <div className="w20p">圖片</div>
+            <div className="w60p disf fd-c jc-se fw6 fs18">
+              <p>訂單編號：</p>
+              <p>店名</p>
+              <p>2022年11月22日</p>
+            </div>
+            <div className="w20p disf fd-c ai-c jc-se gap10">
+              <p className="fw6 fs18">$388</p>
+              <div className="disf fd-c">
+                <p className="oldOrderCommand">重新下單</p>
+                <div>
+                  <p className="oldOrderCommand">給予店家評價</p>
+                </div>
+                <div>
+                  <p className="oldOrderCommand">給予外送評價</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div name="查看細節" className=" padV20 padH20 h50">
+            <p>查看細節(5個品項)</p>
+            <div className="fontMainColor">
+              <i className="fa-solid fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        {/* //===============================================分隔線================================================ */}
+
+        {/* //===============================================分隔線================================================ */}
+      </div>
+    </div>
+  );
 }
 export default MemberOldOrder;
--- src/Temp/test.js
+++ src/Temp/test.js
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function CBtest() {
+  const length = 5;
   const [checks, setChecks] = useState([false, false, false, false, false]);
   const [lengthNow, setLengthNow] = useState(0);
   const limit = 3;
+  // useEffect(() => {
+  //   const newChecks = Array(5).map(() => {
+  //     if (true) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+  //   setChecks(newChecks);
+  // }, []);
   return (
     <>
       {Array(5)
@@ -15,13 +26,13 @@ function CBtest() {
               disabled={lengthNow === limit && !checks[i] ? true : false}
               checked={checks[i]}
               onChange={(e) => {
-                if (!e.target.checked) {
+                if (checks[i]) {
                   const newArr = [...checks];
                   newArr[i] = false;
                   // console.log(lengthNow);
                   setChecks(newArr);
                   setLengthNow(lengthNow - 1);
-                } else if (lengthNow < limit && e.target.checked) {
+                } else if (lengthNow < limit && !checks[i]) {
                   const newArr = [...checks];
                   newArr[i] = true;
                   setChecks(newArr);
