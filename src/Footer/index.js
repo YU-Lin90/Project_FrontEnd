99cebf2813000dc040f8ccd52ad19f2df6f8d4a8
YuChenLin <andy50152@gmail.com>
2022-11-25 01:27:07 +0800

    1125/0127

---
+++ src/Footer/index.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer className="h200 w100p footerBox">
+        <div className="flexSetCenter">Footer</div>
+        <div className="h100 bigHidden w100p"></div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
