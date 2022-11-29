13375a40c47af38a9346cd5e8f8de0e3b8557e13
Leo mini <a230711@yahoo.com.tw>
2022-11-29 23:03:22 +0800

    取餐與完成按鈕

--- src/Deliver/DeliverMessager/index.js
+++ src/Deliver/DeliverMessager/index.js
@@ -3,19 +3,27 @@ import axios from 'axios';
 function DeliverMessager() {
   const ordersid = localStorage.getItem('order_sid');
   async function foodget() {
-    const respon = await axios.put(
-      `http://localhost:3005/deliverorder/${ordersid}`
-    );
+    await axios.put(`http://localhost:3001/deliverorder/${ordersid}`);
+  }
+  async function foodreach(){
+    await axios.put(`http://localhost:3001/finishdeliverorder/${ordersid}`);
   }
 
   return (
     <>
       <button type='button'
         onClick={()=>{
-          foodget()
+          foodget();
         }}
       >
       已取餐
+      </button>
+      <button type='button'
+        onClick={()=>{
+          foodreach();
+        }}
+      >
+        已送達
       </button>
     </>
   );
