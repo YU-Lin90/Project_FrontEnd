aee9775f4b34ee94dc9d5f235e0791cccbd74d75
Leo mini <a230711@yahoo.com.tw>
2022-12-02 13:17:09 +0800

    微調

--- src/Deliver/DeliverLogin/index.js
+++ src/Deliver/DeliverLogin/index.js
@@ -5,9 +5,7 @@ function DeliverLogin() {
 
   return (
     <>
-      <MemberLogin name="外送員登入" account="deliver@test.com" password="123456" kind="Deliver">
-      
-      </MemberLogin>
+      <MemberLogin name="外送員登入" account="deliver@test.com" password="123456" kind="Deliver" />
     </>
   );
 }
