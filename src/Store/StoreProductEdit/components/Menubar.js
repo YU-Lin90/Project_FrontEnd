8d869e9249f6f1bb9ad5f1794904a54d3b044fd6
Aiden4939 <jl4460702@gmail.com>
2022-12-05 05:33:02 +0800

    修正menubar沒有正確顯示的問題

--- src/Store/StoreProductEdit/components/Menubar.js
+++ src/Store/StoreProductEdit/components/Menubar.js
@@ -7,37 +7,49 @@ function Menubar() {
     <>
       <div className="store-admin">
         <ul className="menubar">
-          <li
-            onClick={() => {
-              setPageNow('總覽');
-            }}
-            className={pageNow === '總覽' ? 'active' : ''}
-          >
-            <Link to="/Store/StoreTypeEdit">總覽</Link>
+          <li>
+            <Link
+              onClick={() => {
+                setPageNow('總覽');
+              }}
+              className={pageNow === '總覽' ? 'active' : ''}
+              to="/Store/StoreTypeEdit"
+            >
+              總覽
+            </Link>
           </li>
-          <li
-            onClick={() => {
-              setPageNow('類別');
-            }}
-            className={pageNow === '類別' ? 'active' : ''}
-          >
-            <Link to="/Store/StoreTypeEdit/type">類別</Link>
+          <li>
+            <Link
+              onClick={() => {
+                setPageNow('類別');
+              }}
+              className={pageNow === '類別' ? 'active' : ''}
+              to="/Store/StoreTypeEdit/type"
+            >
+              類別
+            </Link>
           </li>
-          <li
-            onClick={() => {
-              setPageNow('餐點');
-            }}
-            className={pageNow === '餐點' ? 'active' : ''}
-          >
-            <Link to="/Store/StoreTypeEdit/product">餐點</Link>
+          <li>
+            <Link
+              onClick={() => {
+                setPageNow('餐點');
+              }}
+              className={pageNow === '餐點' ? 'active' : ''}
+              to="/Store/StoreTypeEdit/product"
+            >
+              餐點
+            </Link>
           </li>
-          <li
-            onClick={() => {
-              setPageNow('客製化選項');
-            }}
-            className={pageNow === '客製化選項' ? 'active' : ''}
-          >
-            <Link to="/Store/StoreTypeEdit/option">客製化選項</Link>
+          <li>
+            <Link
+              onClick={() => {
+                setPageNow('客製化選項');
+              }}
+              className={pageNow === '客製化選項' ? 'active' : ''}
+              to="/Store/StoreTypeEdit/option"
+            >
+              客製化選項
+            </Link>
           </li>
         </ul>
       </div>
