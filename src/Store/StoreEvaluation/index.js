e9e54cba2f97de749d11c813ee9bb7b2d9a79a97
YuChenLin <andy50152@gmail.com>
2022-12-09 19:54:13 +0800

    瀑布流獨立頁面

--- src/Store/StoreEvaluation/index.js
+++ src/Store/StoreEvaluation/index.js
@@ -1,17 +1,21 @@
 //店家評價 瀑布流 完成
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './Evaluation.css';
 import { useFunc } from '../../Context/FunctionProvider';
 import { useEffect } from 'react';
-function StoreEvaluation({ shopSid = 89 }) {
+import { useNavigate } from 'react-router-dom';
+
+function StoreEvaluation() {
+  const navi = useNavigate();
+  const evaFrameForRef = useRef(null);
   const { notLoginGetFetch } = useFunc();
-  const [allData, setAllData] = useState([]);
   const [firstColumn, setFirstColumn] = useState([]);
   const [secondColumn, setSecondColumn] = useState([]);
   const [thirdColumn, setThirdColumn] = useState([]);
   const [pageNow, setPageNow] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
   const [startLoad, setStartLoad] = useState(true);
+  const [shopSid, setShopSid] = useState(0);
   //獲得總頁數
   const getTotalPages = async () => {
     const res = await notLoginGetFetch(
@@ -43,7 +47,6 @@ function StoreEvaluation({ shopSid = 89 }) {
     setFirstColumn(arrays[0]);
     setSecondColumn(arrays[1]);
     setThirdColumn(arrays[2]);
-    setAllData(res);
     setStartLoad(false);
     // setFirstColumn(Array(pageNow).fill(1));
     // setPageNow((v) => v + 1);
@@ -65,7 +68,8 @@ function StoreEvaluation({ shopSid = 89 }) {
     const scrollTop = scrollNow + screenHeight;
     if (
       bodyHeight > screenHeight &&
-      scrollTop > bodyHeight - 100 &&
+      //                    改這裡的數字可以改判斷點
+      scrollTop > bodyHeight - 400 &&
       !startLoad &&
       pageNow <= totalPage
     ) {
@@ -75,6 +79,13 @@ function StoreEvaluation({ shopSid = 89 }) {
     }
   };
   useEffect(() => {
+    const usp = new URLSearchParams(window.location.search);
+    const uspShopSid = usp.get('shopSid');
+    if (!uspShopSid) {
+      navi(-1);
+    } else {
+      setShopSid(uspShopSid);
+    }
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -82,50 +93,117 @@ function StoreEvaluation({ shopSid = 89 }) {
   }, [startLoad]);
   //頁數增加的時候反應
   useEffect(() => {
-    if (totalPage !== 0) {
+    if (totalPage !== 0 && shopSid !== 0) {
       getEva();
-    } else if (totalPage === 0) {
+    } else if (totalPage === 0 && shopSid !== 0) {
       getTotalPages();
     }
-  }, [totalPage, pageNow]);
+  }, [totalPage, pageNow, shopSid]);
   return (
-    <>
-      <div>
+    <div className="evaBack">
+      <div ref={evaFrameForRef} className="evaFrame">
+        {/* TODO: 評價字串做成固定位置 加上返回鍵 */}
         <p className="ta-c fs36 fw6 padH20 padV20">店家評價</p>
-        <div className="storeEvaluationOuter">
-          <div className="w33p padH10 padV10 disf fd-c jc-c ai-c">
+        <div className="storeEvaluationOuterFrame">
+          <div className="w33p padH20 padV10 disf fd-c ai-c">
             {firstColumn.map((v, i) => {
               return (
-                <div className="w100p h200 bgcMain" key={i}>
-                  {v.sid}
-                  {v.evaluation_content}
+                <div className=" evaCards" key={v.sid}>
+                  <p>
+                    {Array(v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-solid fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                    {Array(5 - v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-regular fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                  </p>
+                  <p className="fs24 fw6">{v.name ? v.name : '匿名訪客'}</p>
+                  <p
+                    style={{
+                      textIndent: `${
+                        v.evaluation_content.length > 20 ? '2em' : 'none'
+                      }`,
+                    }}
+                    className="padH15 contentIndent"
+                  >
+                    {v.evaluation_content}
+                  </p>
+                  <p>{v.evaluation_time}</p>
                 </div>
               );
             })}
           </div>
-          <div className="w33p padH10 padV10 disf fd-c jc-c ai-c">
+          <div className="w33p padH20 padV10 disf fd-c ai-c">
             {secondColumn.map((v, i) => {
               return (
-                <div className="w100p h200 bgcMain" key={i}>
-                  {v.sid}
-                  {v.evaluation_content}
+                <div className=" evaCards" key={v.sid}>
+                  <p>
+                    {Array(v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-solid fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                    {Array(5 - v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-regular fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                  </p>
+                  <p className="fs24 fw6">{v.name ? v.name : '匿名訪客'}</p>
+                  <p className="padH15"> {v.evaluation_content}</p>
+                  <p>{v.evaluation_time}</p>
                 </div>
               );
             })}
           </div>
-          <div className="w33p padH10 padV10 disf fd-c jc-c ai-c">
+          <div className="w33p padH20 padV10 disf fd-c  ai-c">
             {thirdColumn.map((v, i) => {
               return (
-                <div className="w100p h200 bgcMain" key={i}>
-                  {v.sid}
-                  {v.evaluation_content}
+                <div className=" evaCards" key={v.sid}>
+                  <p>
+                    {Array(v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-solid fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                    {Array(5 - v.evaluation_score)
+                      .fill(1)
+                      .map((v, i) => (
+                        <i
+                          key={i}
+                          className="fa-regular fa-star fs18 fontMainColor"
+                        ></i>
+                      ))}
+                  </p>
+                  <p className="fs24 fw6">{v.name ? v.name : '匿名訪客'}</p>
+                  <p className="padH15">{v.evaluation_content}</p>
+                  <p>{v.evaluation_time}</p>
                 </div>
               );
             })}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 export default StoreEvaluation;
