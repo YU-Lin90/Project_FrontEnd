47b4df8f051a3d0c626b0bac706ad47327012e17
YuChenLin <andy50152@gmail.com>
2022-12-09 13:53:15 +0800

    1209/1353

---
+++ src/Store/StoreEvaluation/index.js
@@ -0,0 +1,11 @@
+function StoreEvaluation({ storeSid = 89 }) {
+  return (
+    <>
+      <div>
+        <p>店家評價</p>
+        <div className="storeEvaluationOuter"></div>
+      </div>
+    </>
+  );
+}
+export default StoreEvaluation;
